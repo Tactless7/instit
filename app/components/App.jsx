470621d0c880e13cf3e3195d912371106895b6e5
Juliane Blier <blier.juliane@gmail.com>
2017-01-04 14:05:41 +0100

    Création formulaire Maths

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,13 +4,30 @@ export default class App extends React.Component {
   render() {
     return (
       <div id="content">
-        <h1>&nbsp;</h1>
-        <h2>Welcome!</h2>
-        <ul>
-          <li><a href="http://brunch.io">Brunch homepage</a></li>
-          <li><a href="https://facebook.github.io/react/">React.js homepage</a></li>
-        </ul>
+        <nav>
+          <ul>
+            <li><button>Calcul Mental</button></li>
+            <li><button>Conjugaison</button></li>
+          </ul>
+        </nav>
+        <div id="settings"><Calcul /></div>
       </div>
     );
+  }
+}
+
+class Calcul extends React.Component {
+
+  render(){
+    return (
+      <form>
+        <label htmlFor="seriesNumber">Nombre de séries : <input name="seriesNumber" id="seriesNumber" type="number" /></label>
+        <label htmlFor="calculNumber">Séries de :<input name="calculNumber" id="calculNumber" type="number" /></label>
+        <span>Opérations : </span>
+        <label><input id="addition" value="addition" type="checkbox" />Addition</label>
+        <label><input id="substraction" value="substraction" type="checkbox" />Soustraction</label>
+        <button type="submit" id="generate">Lancer</button>
+      </form>
+    )
   }
 }
