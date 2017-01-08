7da280da5935e6605d4eafa75a5349097ea2910c
Juliane Blier <blier.juliane@gmail.com>
2017-01-08 02:55:42 +0100

    Création du composant Addition

--- app/components/Calcul.jsx
+++ app/components/Calcul.jsx
@@ -29,28 +29,59 @@ class SeriesNumber extends React.Component {
 
   handleChange(event){
     this.setState({value: event.target.value});
+    console.log(this.state);
   }
 
   render(){
     return (
       <label htmlFor="seriesNumber">Nombre de séries : 
-      <input name="seriesNumber" id="seriesNumber" type="number" value={this.state.value} onChange={this.handleChange} /></label>
+        <input name="seriesNumber" id="seriesNumber" type="number" value={this.state.value} onChange={this.handleChange} />
+      </label>
     )
   }
 }
 
 class CalculNumber extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value: 0};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+    console.log(this.state);
+  }
+
   render(){
     return (
-      <label htmlFor="calculNumber">Séries de :<input name="calculNumber" id="calculNumber" type="number" /></label>
+      <label htmlFor="calculNumber">Séries de :
+        <input name="calculNumber" id="calculNumber" type="number" value={this.state.value} onChange={this.handleChange}/>
+      </label>
     )
   }
 }
 
 class Addition extends React.Component {
+  constructor(props){
+    super();
+    this.state = {checked: false};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    if(this.state.checked){
+      this.setState({checked : false});
+    } else {
+      this.setState({checked: true});
+    }
+  }
+
   render(){
     return (
-      <label><input id="addition" value="addition" type="checkbox" />Addition</label>
+      <label><input id="addition" value="addition" type="checkbox" onChange={this.handleChange} />Addition</label>
     )
   }
 }
